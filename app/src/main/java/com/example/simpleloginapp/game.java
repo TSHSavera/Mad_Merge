688cc0d9f38d33c6111251ec77c6565999657fd3
Paul Sandoval <sunghayuki30@gmail.com>
2024-12-25 06:09:36 +0800

    actual game XML added

---
+++ app/src/main/java/com/example/simpleloginapp/game.java
@@ -0,0 +1,27 @@
+package com.example.simpleloginapp;
+
+import android.os.Bundle;
+
+import androidx.activity.ComponentActivity;
+import androidx.activity.EdgeToEdge;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+public class game extends ComponentActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_game);
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.layout_game), (v, insets) -> {
+            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+            return insets;
+        });
+        startUp();
+    }
+    private void startUp() {
+
+    }
+}
