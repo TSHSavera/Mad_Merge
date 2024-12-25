6c9b94fd15449e8664cde508d3b08311fe4c1c59
Paul Sandoval <sunghayuki30@gmail.com>
2024-12-25 11:13:01 +0800

    user account section added

--- app/src/main/java/com/example/simpleloginapp/activity_user.java
+++ app/src/main/java/com/example/simpleloginapp/activity_user.java
@@ -1,5 +1,6 @@
 package com.example.simpleloginapp;
 
+import android.annotation.SuppressLint;
 import android.os.Bundle;
 
 import androidx.activity.ComponentActivity;
@@ -9,12 +10,14 @@ import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
 public class activity_user extends ComponentActivity {
+
+    @SuppressLint("MissingInflatedId")
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_game);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.layout_game), (v, insets) -> {
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.layout_user), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
