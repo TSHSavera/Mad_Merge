6953c89bbf098ee1ebbcc6f8021ffbbdf355f86d
Paul Sandoval <sunghayuki30@gmail.com>
2024-12-26 14:17:37 +0800

    saved game section added

--- app/src/main/java/com/example/simpleloginapp/saved_game.java
+++ app/src/main/java/com/example/simpleloginapp/saved_game.java
@@ -9,15 +9,15 @@ import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
-public class activity_user extends ComponentActivity {
+public class saved_game extends ComponentActivity {
 
     @SuppressLint("MissingInflatedId")
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
-        setContentView(R.layout.activity_user);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.layout_user), (v, insets) -> {
+        setContentView(R.layout.activity_saved_game);
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.layout_savedGame), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
